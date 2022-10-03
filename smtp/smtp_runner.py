feced40caa9a52992d55cf70f2713795d6bc30bd
Kirill Matveev <kirill.matveev@gt-m.ru>
2022-10-03 22:43:53 +0300

    Refactoring frontend and smtp server

--- smtp/smtp_runner.py
+++ smtp/smtp_runner.py
@@ -4,9 +4,18 @@ from smtp_controller import SMTPController
 from smtp_handler import SMTPHandler
 
 from utils.logger import smtp_logger
+from utils.envs import validate_envs, set_default_environs
 
 
 if __name__ == "__main__":
+
+    # Set default to environ
+    set_default_environs()
+
+    # Validate environ variables
+    validate_envs(os.environ)
+
+    # Configure SMTP-controller
     controller = SMTPController(SMTPHandler(), hostname=os.getenv('SMTP_HOSTNAME'), port=os.getenv('SMTP_PORT'))
 
     try:
@@ -16,7 +25,7 @@ if __name__ == "__main__":
         while True:
             pass
     except KeyboardInterrupt:
-        pass
+        exit(0)
     finally:
         smtp_logger.configure('Preparing to shut down the server')
         controller.stop()
---
+++ smtp/utils/envs.py
@@ -0,0 +1,23 @@
+import os
+
+
+def validate_envs(environ_variables):
+    variables = list(environ_variables)
+    required_variables = ["SMTP_HOSTNAME", "SMTP_PORT", "BACKEND_HOSTNAME", "BACKEND_PORT"]
+
+    for variable in required_variables:
+        if not (variable in variables and os.environ.get(variable, default="") != ""):
+            raise EnvironmentError(
+                f"Required environment not set: {variable}"
+            )
+
+
+def set_default_environs():
+    required_variables = {
+        'SMTP_HOSTNAME': '0.0.0.0',
+        'BACKEND_HOSTNAME': '0.0.0.0',
+        'BACKEND_PORT': "0"
+    }
+
+    for k, v in required_variables.items():
+        os.environ[k] = v
